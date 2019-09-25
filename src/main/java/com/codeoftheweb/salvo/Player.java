287d16f14679ad77162229fbb8b9ca6012f4213d
Foras <juanse.stuven.96@hotmail.com>
2019-09-25 15:10:16 -0300

    task 1

---
+++ src/main/java/com/codeoftheweb/salvo/Player.java
@@ -0,0 +1,32 @@
+package com.codeoftheweb.salvo;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Player {
+    private String userName;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getuserName() {
+        return userName;
+    }
+
+    public Player() { }
+
+    public Player(String userName) {
+        this.userName = userName;
+    }
+
+}
